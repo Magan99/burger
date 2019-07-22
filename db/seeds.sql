bc0d82d79157195e11f9748e1205dab3bb580334
Mdaher <mdaher612@gmail.com>
2019-07-21 23:29:06 -0500

    The server closed the connection

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers(burger_name,devoured) VALUES('chicken burger', false); 
+INSERT INTO burgers(burger_name,devoured)VALUES('camel burger', false);
+INSERT INTO burgers(burger_name,devoured) VALUES ('vegan', false)
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Veggie Burger', true);
